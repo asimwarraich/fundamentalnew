da91b9078fa0b2996c8cb1adb2bf70b971e4940d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-10-05 09:43:12 +0500

    optimizations

---
+++ src/screens/Jumbotron.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import videoone from "../img/videoone.mp4";
+import banner from "../img/banner.png";
+
+export function Jumbotron() {
+  const [isVideo, setIsVideo] = React.useState(false);
+  return (
+    <div className="landing__main__banner">
+      <div className="video__pause__wraper">
+        {isVideo ? (
+          <video src={videoone} type="video/mp4" id="video" />
+        ) : (
+          <img src={banner} alt="banner" />
+        )}
+        <div className="main__video__btn__container">
+          <button
+            className="main__video__btn"
+            onClick={() => {
+              setIsVideo(true);
+              const video = document.getElementById("video");
+              if (video.paused) video.play();
+              else video.pause();
+              document.querySelector(".main__video__btn").style.display =
+                "none";
+            }}
+          >
+            <svg width="36" height="41" viewBox="0 0 36 41" fill="none">
+              <path
+                d="M34.5 17.9019C36.5 19.0566 36.5 21.9434 34.5 23.0981L5.25001 39.9856C3.25001 41.1403 0.750002 39.6969 0.750002 37.3875L0.750003 3.61251C0.750004 1.30311 3.25 -0.140273 5.25 1.01443L34.5 17.9019Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -7,37 +7,14 @@ import qua from "../img/qua.png";
 import quaover from "../img/quaover.png";
 import Footer from "../components/Footer";
 import HomeProductsCard from "../components/HomeProductsCard";
-import videoone from "../img/videoone.mp4";
 import { LandingServicesSection } from "./LandingServicesSection";
+import { Jumbotron } from "./Jumbotron";
 
 export default function Landing() {
   return (
     <>
       <Header />
-      <div className="landing__main__banner">
-        <div className="video__pause__wraper">
-          <video src={videoone} type="video/mp4" id="video" />
-          <div className="main__video__btn__container">
-            <button
-              className="main__video__btn"
-              onClick={() => {
-                const video = document.getElementById("video");
-                if (video.paused) video.play();
-                else video.pause();
-                document.querySelector(".main__video__btn").style.display =
-                  "none";
-              }}
-            >
-              <svg width="36" height="41" viewBox="0 0 36 41" fill="none">
-                <path
-                  d="M34.5 17.9019C36.5 19.0566 36.5 21.9434 34.5 23.0981L5.25001 39.9856C3.25001 41.1403 0.750002 39.6969 0.750002 37.3875L0.750003 3.61251C0.750004 1.30311 3.25 -0.140273 5.25 1.01443L34.5 17.9019Z"
-                  fill="white"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Jumbotron />
       <div className="landing__about__section">
         <div className="about__overlay__contant__wraper">
           <div className="about__overlay__laft__contant">
